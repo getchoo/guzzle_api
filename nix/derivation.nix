9c0e78b1b9a363dc2221a4d1861e33ced38744fe
seth <getchoo@tuta.io>
2023-09-28 05:18:34 -0400

    fix(flake): use pyproject format in buildPythonPackage
    see https://github.com/NixOS/nixpkgs/pull/254309

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -6,26 +6,15 @@
   pydantic,
   uvicorn,
   self,
-  build,
-  wheel,
 }:
 buildPythonPackage {
   pname = "guzzle-api";
   version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
 
   src = lib.cleanSource self;
-  format = "flit";
+  format = "pyproject";
   doCheck = false;
 
-  dontUsePypaBuild = true;
-
-  # TODO: find out what's going on with pypaBuildHook
-  buildPhase = ''
-    runHook preBuild
-    pyproject-build --no-isolation --outdir dist/ --wheel $pypaBuildFlags
-    runHook postBuild
-  '';
-
   propagatedBuildInputs = [fastapi pydantic uvicorn];
-  nativeBuildInputs = [flit-core build wheel];
+  nativeBuildInputs = [flit-core];
 }
